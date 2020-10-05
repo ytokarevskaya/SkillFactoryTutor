eabe7efbde35413af0f4798da27ecf80a319f6bc
ytokarevskaya <lan2214@yandex.ru>
2020-10-05 21:31:46 +0300

    Проверено

--- module12_homework/Task2.js
+++ module12_homework/Task2.js
@@ -3,3 +3,9 @@ funcHasProperty = (str,obj) => {
         return key === str ? console.log(true) : console.log(false);
     }    
 };
+
+// Задание выполнено верно, но есть более простой способ проверить наличие свойства в объекте, без перебора всех ключей (ведь их может быть очень много). Более оптимальный способ решения такой:
+
+function checkPropInObj (str, obj) {
+  return (str in obj); // возвращает true или false
+}
--- module12_homework/Task4.js
+++ module12_homework/Task4.js
@@ -3,17 +3,17 @@ function ElectricalAppliances(category, model, amperage, color) {
     this.model = model;
     this.amperage = amperage;
     this.color = color;
-    statusOnOff = false;
+    this.statusOnOff = false;
     this.open = false;
 }
 
 ElectricalAppliances.prototype.switchOnOff = function () { //вкл/выкл и показатель потребляемой мощности
-    if(statusOnOff == false) {
+    if(this.statusOnOff == false) {
         console.log(`${this.model} включен`);
-        statusOnOff = true;
+        this.statusOnOff = true;
         console.log(`Потребляемая мощность ${this.amperage*220} ватт`);
     } else {
-        statusOnOff = false;
+        this.statusOnOff = false;
         console.log(`${this.model} выключен`);
         console.log(`Потребляемая мощность 0 ватт`);
     }
@@ -29,38 +29,74 @@ ElectricalAppliances.prototype.openDoors = function () {
     }
 };
 
-const refrigerator = new ElectricalAppliances("Товары для кухни", "Холодильник Samsung", 2, "white"),
-    microwave = new ElectricalAppliances("Товары для кухни", "Микроволновка Braun", 0, "black");
 
-refrigerator.storageForFood = 100; //Холодильник пуст на 100%
-refrigerator.food = new Map(); //Массив продуктов в холодильнике
+function Refrigerator(category, model, amperage, color) {
+    this.category = category;
+    this.model = model;
+    this.amperage = amperage;
+    this.color = color;
+    this.statusOnOff = false;
+    this.open = false;
+    this.storageForFood = 100; //Холодильник пуст на 100%
+    this.food = new Map(); //Массив продуктов в холодильнике
+}
+
+Refrigerator.prototype = new ElectricalAppliances(); // в этой строчке как создаем делегирующую связь между родительской и дочерней функцией
 
 //Добавить продукты в холодильник
-refrigerator.addFood = function (food) {
-    if(refrigerator.open) {
-        if(refrigerator.storageForFood >= 0) {
+Refrigerator.prototype.addFood = function (food) {
+    if(this.open) {
+        if(this.storageForFood >= 0) {
             let sizeFood = Math.round((Math.random(1)+0.1)*30); //Размер пищи, допустим, произвольный
-            refrigerator.food.set(food,sizeFood);
-            refrigerator.storageForFood -= sizeFood;
-            console.log(`В ${refrigerator.model} добавлено "${food}"`);
+            this.food.set(food,sizeFood);
+            this.storageForFood -= sizeFood;
+            console.log(`В ${this.model} добавлено "${food}"`);
         } else {
-            console.log(`${refrigerator.model} переполнен`);
+            console.log(`${this.model} переполнен`);
         }
     } else {
-        console.log(`${refrigerator.model} сначала бы не помешало открыть`);
+        console.log(`${this.model} сначала бы не помешало открыть`);
     }
 };
 
 //Взять продукты из холодильника
-refrigerator.takeFood = function (food)  {
-     refrigerator.food.forEach((sizeFood,name) => {
+Refrigerator.prototype.takeFood = function (food)  {
+     this.food.forEach((sizeFood,name) => {
         if(name == food) {
-            refrigerator.food.delete(name);
+            this.food.delete(name);
             console.log(`Из холодильника взято "${name}"`);
-            refrigerator.storageForFood += sizeFood;
+            this.storageForFood += sizeFood;
         } 
     });
 };
+
+function Microwave(category, model, amperage, color, bookOfRecipes) {
+    this.category = category;
+    this.model = model;
+    this.amperage = amperage;
+    this.color = color;
+    this.statusOnOff = false;
+    this.open = false;
+    this.bookOfRecipes = bookOfRecipes;
+}
+
+Microwave.prototype = new ElectricalAppliances();
+
+Microwave.prototype.heatFood = function (time, food = "Пустая микроволновка") { //время подогрева пищи (в сек.)
+    if(this.statusOnOff) {
+        if(this.open) {
+            function timeOut() {
+                time -= 1;
+                time*1000-1000 >= 0 ? console.log(time,`Потребляемая мощность ≈ 1300 ватт`) : console.log(`*Дзыынь* Блюдо "${food}" готово!`);
+            }
+            let intId = setInterval(timeOut,1000);
+            setTimeout(clearInterval,time*1000,intId);
+        }
+    } 
+};
+
+const refrigerator = new Refrigerator("Товары для кухни", "Холодильник Samsung", 2, "white"),
+    microwave = new Microwave("Товары для кухни", "Микроволновка Braun", 0, "black", true);
 
 //Тест холодильника
 
@@ -78,22 +114,6 @@ refrigerator.takeFood = function (food)  {
 // refrigerator.addFood("Ананасы");
 // refrigerator.switchOnOff();
 
-
-let intId;
-microwave.bookOfRecipes = true;
-microwave.heatFood = function (time, food = "Пустая микроволновка") { //время подогрева пищи (в сек.)
-    if(statusOnOff) {
-        if(microwave.open) {
-        function timeOut() {
-            time -= 1;
-            time*1000-1000 >= 0 ? console.log(time,`Потребляемая мощность ≈ 1300 ватт`) : console.log(`*Дзыынь* Блюдо "${food}" готово!`);
-        }
-        intId = setInterval(timeOut,1000);
-        setTimeout(clearInterval,time*1000,intId);
-    }
-} 
-};
-
 //Тест микроволновки
 
 // microwave.switchOnOff();
@@ -105,3 +125,8 @@ microwave.heatFood = function (time, food = "Пустая микроволнов
 // setTimeout(() => {microwave.switchOnOff()}, 15000);
 
 
+// Хорошая работа, плюс за креативность ;) но есть несколько серьёзных недочётов:
+// 1. В ElectricalAppliances свойство statusOnOff не присваивается объекту, а просто объявляется отдельной переменной. Т.к. перед переменной нет ключевого слова (var/let/const), она ещё и становится глобальной. Т.е. у вас одно свойство вкл./выкл для всех объектов, грубо говоря, выключив холодильник, у вас бы выключились заодно и все остальные приборы, что является довольно странным поведением :) Кроме того, использовать глобальные переменные в принципе не рекомендуется (за исключением случаев, когда это действительно необходимо), т.к. вы можете случайно переписать какие-то важные системные параметры на странице.
+// 2. По заданию нужно было создать делегирующую связь между родительской функцией-конструкторами (ElectricalAppliances) и дочерними. У вас не определены дочерние конструкторы, приборы холодильник и микроволновка являются просто экземплярами родительской функции, что не совсем правильно. Те методы и свойства, которые вы задали для этих приборов (addFood, heatFood, takeFood и т.д.) существуют только в этих 2-х созданных экземплярах. Если вы захотите создать например ещё один холодильник, вам придётся заново присваивать ему эти же самые свойства и методы, что не очень удобно. Для этого и нужно создать конструктор, чтобы свойства и методы, присущие каким-то типичным объектам присваивались им по умолчанию при создании.
+
+// В коде постаралась исправить вышеперечисленные проблемы, сохранив суть.
